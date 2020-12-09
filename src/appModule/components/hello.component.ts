a4673e5cb2644d25ddd2033e8b905a6857ff6253
MahitaAndeTR <66035187+MahitaAndeTR@users.noreply.github.com>
2020-12-09 07:22:15 -0600

    Update hello.component.ts

--- src/appModule/components/hello.component.ts
+++ src/appModule/components/hello.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 import { CompositeInjector } from '../services/composite-injector.service';
 import { NotificationEmitter } from '../services/notification-emitter.service';
